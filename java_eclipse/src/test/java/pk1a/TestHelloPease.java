0f22f17e55a90d1f746e5a28168b750de1f74df9
innayan <inna_yan@mail.ru>
2020-12-29 11:04:27 +0300

    Update TestHelloPease.java

--- java_eclipse/src/test/java/pk1a/TestHelloPease.java
+++ java_eclipse/src/test/java/pk1a/TestHelloPease.java
@@ -6,7 +6,18 @@ import junit.framework.*;
 //fffff
 public class  TestHelloPease extends TestCase {
   public void testSimpleMessage() {
+  System.println("##teamcity[testStarted name='TestHelloPease']")
+   	try 
+{
+    Thread.sleep(60000);
+} 
+catch(InterruptedException e)
+{
+     // this part is executed when an exception (in this example InterruptedException) occurs
+}
      String message = new HelloPease("World222").sayHello();
      Assert.assertEquals("Test Hello Machine", "Hello, World222", message);
-  }
+   System.println("##teamcity[testFinished  name='TestHelloPease']")
+   
+     }
 }
